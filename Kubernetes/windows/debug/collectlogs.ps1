61d274f4e84cdbc66a7e9569947e21a4b003b61a
Patrick Lang <plang@microsoft.com>
2018-08-02 15:36:21 -0700

    DO NOT MERGE using temporary github URL

--- Kubernetes/windows/debug/collectlogs.ps1
+++ Kubernetes/windows/debug/collectlogs.ps1
@@ -8,7 +8,7 @@ md $BaseDir -ErrorAction Ignore
 $helper = "$BaseDir\helper.psm1"
 if (!(Test-Path $helper))
 {
-    Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/helper.psm1 -OutFile $BaseDir\helper.psm1
+    Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/PatrickLang/SDN/patch-1-temp/Kubernetes/windows/helper.psm1 -OutFile $BaseDir\helper.psm1
 }
 ipmo $helper
 
